682823f28ea3f182b08e5a73a2716cdb4b0f689b
Rafael Dohms <rdohms@gmail.com>
2012-04-19 23:38:01 +0200

    Updating library to use Composer in dev-mode also Updates Travis support Removed unneeded bootstrap and class loaders Updated README Moved phpunit.xml to root

--- tests/Tests/Testcase.php
+++ tests/Tests/Testcase.php
@@ -7,29 +7,27 @@ use DMS\Filter\Mapping,
 
 class Testcase extends \PHPUnit_Framework_TestCase
 {
-    
     protected $loader;
-    
+
     public function setUp()
     {
         parent::setUp();
     }
-    
+
     public function tearDown()
     {
         parent::tearDown();
     }
-    
+
     protected function buildMetadataFactory()
     {
         $reader = new Annotations\AnnotationReader();
-        $reader->setEnableParsePhpImports(true);
-        
+
         $loader = new Mapping\Loader\AnnotationLoader($reader);
         $this->loader = $loader;
-        
+
         $metadataFactory = new Mapping\ClassMetadataFactory($loader);
-        
+
         return $metadataFactory;
     }
 }
