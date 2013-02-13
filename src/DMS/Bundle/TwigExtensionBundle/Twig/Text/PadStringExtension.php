096cf2812f93d5a7e0567ba04bdaf39a4d4bad82
Kim Rowan <kim.rowan@cancer.org.uk>
2013-02-13 14:25:08 +0000

    Add check to ensure second argument is an integer

--- src/DMS/Bundle/TwigExtensionBundle/Twig/Text/PadStringExtension.php
+++ src/DMS/Bundle/TwigExtensionBundle/Twig/Text/PadStringExtension.php
@@ -40,6 +40,10 @@ class PadStringExtension extends \Twig_Extension
      */
     public function padStringFilter($value, $padCharacter, $maxLength, $padLeft = true)
     {
+        if ( ! is_int($maxLength) ) {
+            throw new \InvalidArgumentException('Pad String Filter expects its second argument to be an integer');
+        }
+
         if (function_exists('mb_strlen')) {
             $padLength = $maxLength - mb_strlen($value);
         } else {
